5614b4a1134b8bc7235d016f886b516061c409fb
Nima Nekouei <nekoueinima@protonmail.com>
2022-06-10 18:25:45 +0430

    feat(edit-modal): edit-modat component created and styled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { TaskComponent } from './task/task.component';
+import { EditModalComponent } from './edit-modal/edit-modal.component';
 
 @NgModule({
-  declarations: [AppComponent, InputComponent, TaskComponent],
+  declarations: [AppComponent, InputComponent, TaskComponent, EditModalComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
