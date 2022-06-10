571b320b96a2fad351fdc9516b330c197ccc1ca4
Nima Nekouei <nekoueinima@protonmail.com>
2022-06-10 16:35:57 +0430

    init(): tailwind installed and configured and needed components added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
